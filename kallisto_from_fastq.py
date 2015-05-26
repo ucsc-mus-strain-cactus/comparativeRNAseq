4302e111e02c20d5cbd40ee16a82fc19b06eb7f6
ifiddes <ian.t.fiddes@gmail.com>
2015-05-26 09:05:53 -0700

    new version to run kallisto straight from fastqs, determining pairing information from filenames

---
+++ kallisto_from_fastq.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+"""
+Kallisto from fastqs.
+
+Hunts through --dir (which is a FTP download from ftp://ftp-mouse.sanger.ac.uk/) and runs kallisto. 
+Designed with 1505 fastq release in mind. The starting directory structure will be copied.
+
+Attempts to determine if the input RNAseq was paired or unpaired and acts accordingly.
+"""
+
+import sys
+import os
+import argparse
+import pysam
+from itertools import izip
+
+from jobTree.scriptTree.target import Target
+from jobTree.scriptTree.stack import Stack
+from jobTree.src.bioio import setLoggingFromOptions, system, logger, fastqRead
+
+def build_parser():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--source_dir", help="Directory containing files.",
+                        default="/hive/groups/recon/projs/mus_strain_cactus/data/assembly_rel_1505/fastqs/"
+                                "ftp-mouse.sanger.ac.uk/REL-1505-RNA-Seq/fastq")
+    parser.add_argument("--reference", help="kallisto reference", 
+                        default="gencode_VM4_Reference/kallisto_reference/tgencode.VM4.idx")
+    parser.add_argument("--out_dir", help="output location (base directory)", 
+                        default="/hive/groups/recon/projs/mus_strain_cactus/pipeline_data/rnaseq/expression")
+    return parser
+
+
+def find_paired_fastqs(source_dir, base_path, files):
+    fastq_files = {}
+    base_filenames = {x.split(".")[0] for x in files}
+    experiments = {x.split("_")[0] for x in base_filenames}
+    for experiment in experiments:
+        if experiment in base_filenames:
+            # this experiment is unpaired
+            path = os.path.join(source_dir, base_path, experiment + ".fastq.gz")
+            assert os.path.exists(path)
+            fastq_files[experiment] = path
+        else:
+            # this experiment is paired
+            fwd = os.path.join(source_dir, base_path, experiment + "_1.fastq.gz")
+            rev = os.path.join(source_dir, base_path, experiment + "_2.fastq.gz")
+            assert all([os.path.exists(fwd), os.path.exists(rev)])
+            fastq_files[experiment] = [fwd, rev]
+    return fastq_files
+
+
+def wrapper(target, source_dir, reference, out_dir):
+    for base_path, dirs, files in os.walk(source_dir):
+        if files:
+            try:
+                genome, rna_seq, institute, tissue = base_path.replace(source_dir, "").split("/")[1:]
+            except ValueError:
+                raise RuntimeError("Looks like the directory structure is not what we expected.")
+            fastq_files = find_paired_fastqs(source_dir, baes_path, files)
+            for experiment, fastq_path in fastq_files.iteritems():
+                try:
+                    fwd_fastq_path, rev_fastq_path = fastq_path
+                    target.addChildTargetFn(kallisto_paired, args=(genome, institute, tissue, reference, out_dir, 
+                                                                   experiment, fwd_fastq_path, rev_fastq_path))
+                except ValueError:
+                    read_size = fastq_read_size(fastq_path)
+                    target.addChildTargetFn(kallisto_single, args=(genome, institute, tissue, reference, out_dir, 
+                                                                   experiment, fastq_path))
+
+
+def kallisto_paired(target, genome, institute, tissue, reference, out_dir, experiment, fwd_fastq_path, rev_fastq_path):
+    out_path = build_out_dirs(out_dir, genome, institute, tissue, experiment)
+    system("kallisto quant -i {} -o {} {} {}".format(reference, out_path, fwd_fastq_path, rev_fastq_path))
+
+
+def kallisto_single(target, genome, institute, tissue, reference, out_dir, experiment, fastq_path, read_size):
+    out_path = build_out_dirs(out_dir, genome, institute, tissue, experiment)
+    system("kallisto quant --single -l {} -i {} -o {} {}".format(read_size, reference, out_path, fastq_path))
+
+
+def build_out_dirs(out_dir, genome, institute, tissue, experiment):
+    out_path = os.path.join(out_dir, genome, institute, tissue, experiment)
+    try:
+        os.makedirs(out_path)
+    except OSError:
+        pass
+    return out_path
+
+
+def fastq_read_size(fastq_path, num_reads=10000):
+    sizes = []
+    fastq_handle = fastqRead(fastq_path)
+    for i in xrange(num_reads):
+        name, seq, qual = fastq_handle.next()
+        sizes.append(len(seq))
+    return 1.0 * sum(sizes) / len(sizes)
+
+
+def main():
+    parser = build_parser()
+    Stack.addJobTreeOptions(parser)
+    args = parser.parse_args()
+    setLoggingFromOptions(args)
+
+    i = Stack(Target.makeTargetFn(wrapper, args=(args.source_dir, args.reference, args.out_dir))).startJobTree(args)
+
+    if i != 0:
+        raise RuntimeError("Got failed jobs")
+
+
+if __name__ == "__main__":
+    from kallisto_remap import *
+    main()
